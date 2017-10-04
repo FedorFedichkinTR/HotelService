9ae6d5a152164f30f4132c9c8728ee771f26fad0
Жанна <Janny.Gorelova@gmail.com>
2017-10-04 19:14:36 +0300

    database DAO interfaces

---
+++ src/main/java/dao/implementation/H2DAOFactory.java
@@ -0,0 +1,25 @@
+package dao.implementation;
+
+import dao.interfaces.*;
+
+public class H2DAOFactory implements AbstractDAOFactory {
+    @Override
+    public UserDAO createUserDAO() {
+        return null;
+    }
+
+    @Override
+    public OrderDAO createOrderDAO() {
+        return null;
+    }
+
+    @Override
+    public RoomDAO createRoomDAO() {
+        return null;
+    }
+
+    @Override
+    public BillDAO createBillDAO() {
+        return null;
+    }
+}
