dc211c478d2959aa6821fc9f81d07d1c56d36fc6
Жанна <Janny.Gorelova@gmail.com>
2017-10-05 15:06:09 +0300

    Order DAO interface

--- src/main/java/dao/interfaces/OrderDAO.java
+++ src/main/java/dao/interfaces/OrderDAO.java
@@ -1,4 +1,15 @@
 package dao.interfaces;
 
+import model.Order;
+
 public interface OrderDAO {
+    public Order create();
+
+    public Order read(long readOrderID);
+
+    public Order update(Order updatedOrder);
+
+    public void delete(Order deletedOrder);
+
+    public void insert(Order insertedOrder);
 }
