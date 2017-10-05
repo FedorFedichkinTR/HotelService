44684138ceae792da3f975965bdaa5df778bb5b6
Жанна <Janny.Gorelova@gmail.com>
2017-10-05 15:03:59 +0300

    Bill DAO interface

--- src/main/java/dao/interfaces/BillDAO.java
+++ src/main/java/dao/interfaces/BillDAO.java
@@ -1,4 +1,15 @@
 package dao.interfaces;
 
+import model.Bill;
+
 public interface BillDAO {
+    public Bill create();
+
+    public Bill read(long billID);
+
+    public void update(Bill bill);
+
+    public void delete(Bill bill);
+
+    public void insert(Bill bill);
 }
