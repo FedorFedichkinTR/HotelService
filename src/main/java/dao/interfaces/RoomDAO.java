0fa564f11ae8276a004cb39a22d3496d14203285
Жанна <Janny.Gorelova@gmail.com>
2017-10-05 15:07:45 +0300

    Room DAO interface

--- src/main/java/dao/interfaces/RoomDAO.java
+++ src/main/java/dao/interfaces/RoomDAO.java
@@ -1,4 +1,15 @@
 package dao.interfaces;
 
+import model.Room;
+
 public interface RoomDAO {
+    public Room create();
+
+    public Room read(long readRoomID);
+
+    public Room update(Room updateRoom);
+
+    public void delete(Room deletedRoom);
+
+    public void insert(Room insertedRoom);
 }
