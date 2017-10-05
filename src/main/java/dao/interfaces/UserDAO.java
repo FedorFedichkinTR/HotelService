d0e3ce4438540ad26550165deec749dd0e1dcc95
Жанна <Janny.Gorelova@gmail.com>
2017-10-05 15:09:28 +0300

    User DAO interface

--- src/main/java/dao/interfaces/UserDAO.java
+++ src/main/java/dao/interfaces/UserDAO.java
@@ -1,4 +1,15 @@
 package dao.interfaces;
 
+import model.User;
+
 public interface UserDAO {
+    public User create();
+
+    public User read(long reaUserID);
+
+    public User update(User updatedUser);
+
+    public void delete(User deletedUser);
+
+    public void insert(User insertedUser);
 }
