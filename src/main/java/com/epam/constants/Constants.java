850e3c6bb86bde92690690334b4e0964336e9f14
Vera <ver9510@yandex.ru>
2017-10-09 12:45:01 +0300

    Added constants fro authorisation and registration services

--- src/main/java/com/epam/constants/Constants.java
+++ src/main/java/com/epam/constants/Constants.java
@@ -4,6 +4,7 @@ package com.epam.constants;
 public class Constants {
     public static final String USER_DAO = "userDao";
     public static final String RELATION_DAO = "relationDao";
-    public static final String USER_SERVICE = "userService";
+    public static final String AUTHORISATION_SERVICE = "authorisationService";
+    public static final String REGISTRATION_SERVICE = "registrationService";
 
 }
