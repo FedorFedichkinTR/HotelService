020005d1daedc2ceca089e0abe4118159d99c98c
Vera <ver9510@yandex.ru>
2017-10-10 12:43:00 +0300

    Added getting all orders from database, which this user owns.

---
+++ src/main/java/com/epam/services/ViewUserOrdersService.java
@@ -0,0 +1,19 @@
+package com.epam.services;
+
+import com.epam.dao.interfaces.AbstractDaoFactory;
+import com.epam.model.Order;
+import com.epam.model.User;
+
+import java.util.List;
+
+public class ViewUserOrdersService {
+    private AbstractDaoFactory daoFactory;
+
+    public ViewUserOrdersService(AbstractDaoFactory daoFactory) {
+        this.daoFactory = daoFactory;
+    }
+
+    public List<Order> getOrdersOfUser(User user){
+        return daoFactory.createOrderDAO().getAllOrdersByUserID(user.getUserID());
+    }
+}
