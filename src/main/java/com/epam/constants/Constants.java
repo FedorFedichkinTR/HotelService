efab249160da815e1a4f56ed8b978c17830c998e
Vera <ver9510@yandex.ru>
2017-10-10 12:44:33 +0300

    Added constants for list of user orders and view of user orders service.

--- src/main/java/com/epam/constants/Constants.java
+++ src/main/java/com/epam/constants/Constants.java
@@ -3,8 +3,10 @@ package com.epam.constants;
 
 public class Constants {
     public static final String USER_SESSION="user";
+    public static final String LIST_OF_USER_ORDERS="listOfUserOrders";
 
     public static final String AUTHORISATION_SERVICE = "authorisationService";
     public static final String REGISTRATION_SERVICE = "registrationService";
     public static final String BOOKING_SERVICE = "bookingService";
+    public static final String VIEW_USER_ORDERS_SERVICE = "viewUserOrdersService";
 }
