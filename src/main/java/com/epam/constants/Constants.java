256b366945e8a761d3459e1ebb69009afbde689c
Vera <ver9510@yandex.ru>
2017-10-10 11:50:55 +0300

    Added constants for Booking service and user session

--- src/main/java/com/epam/constants/Constants.java
+++ src/main/java/com/epam/constants/Constants.java
@@ -2,6 +2,9 @@ package com.epam.constants;
 
 
 public class Constants {
+    public static final String USER_SESSION="user";
+
     public static final String AUTHORISATION_SERVICE = "authorisationService";
     public static final String REGISTRATION_SERVICE = "registrationService";
+    public static final String BOOKING_SERVICE = "bookingService";
 }
