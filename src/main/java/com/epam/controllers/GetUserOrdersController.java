542d9d188199087e1b61d7539082b3e6a0cffc47
Vera <ver9510@yandex.ru>
2017-10-10 11:55:36 +0300

    Empty controller for jsp page myOrders.jsp

---
+++ src/main/java/com/epam/controllers/GetUserOrdersController.java
@@ -0,0 +1,16 @@
+package com.epam.controllers;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/userOrders")
+public class GetUserOrdersController extends HttpServlet{
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
