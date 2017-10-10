87802db011a1ab0534fada4a03f8ab729b18e3e1
Vera <ver9510@yandex.ru>
2017-10-10 19:32:06 +0300

    Added constans for ViewAllOrdersService

--- src/main/java/com/epam/constants/Constants.java
+++ src/main/java/com/epam/constants/Constants.java
@@ -4,9 +4,11 @@ package com.epam.constants;
 public class Constants {
     public static final String USER_SESSION="user";
     public static final String LIST_OF_USER_ORDERS="listOfUserOrders";
+    public static final String LIST_OF_ALL_ORDERS="listOfAllOrders";
 
     public static final String AUTHORISATION_SERVICE = "authorisationService";
     public static final String REGISTRATION_SERVICE = "registrationService";
     public static final String BOOKING_SERVICE = "bookingService";
     public static final String VIEW_USER_ORDERS_SERVICE = "viewUserOrdersService";
+    public static final String VIEW_ALL_ORDERS_SERVICE = "viewAllOrdersService";
 }
