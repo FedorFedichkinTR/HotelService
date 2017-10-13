be9e96f166b92d15b3c88c0ce0fa360951a629fd
Vera <ver9510@yandex.ru>
2017-10-13 18:17:09 +0300

    Added rooms in table

--- src/main/resources/sql/tablecreation.sql
+++ src/main/resources/sql/tablecreation.sql
@@ -44,20 +44,29 @@ VALUES ('tt@epam.com', 'fcea920f7412b5da7be0cf42b8c93759', 'first_name3', 'last_
 /* fcea920f7412b5da7be0cf42b8c93759 is MD5 encrypted password "1234567". */
 
 
-INSERT INTO Rooms (capacity, type, price)
-VALUES (3, 'STANDARD', 200);
-INSERT INTO Rooms (capacity, type, price)
-VALUES (1, 'STANDARD', 180);
-INSERT INTO Rooms (capacity, type, price)
-VALUES (2, 'STANDARD', 190);
-INSERT INTO Rooms (capacity, type, price)
-VALUES (2, 'SUITE', 350);
-INSERT INTO Rooms (capacity, type, price)
-VALUES (2, 'JUNIOR', 250);
-
+INSERT INTO Rooms (capacity, type, price) VALUES (1, 'STANDARD', 150);
+INSERT INTO Rooms (capacity, type, price) VALUES (2, 'STANDARD', 170);
+INSERT INTO Rooms (capacity, type, price) VALUES (3, 'STANDARD', 190);
+INSERT INTO Rooms (capacity, type, price) VALUES (1, 'STANDARD', 150);
+INSERT INTO Rooms (capacity, type, price) VALUES (2, 'STANDARD', 170);
+INSERT INTO Rooms (capacity, type, price) VALUES (3, 'STANDARD', 190);
+INSERT INTO Rooms (capacity, type, price) VALUES (1, 'SUITE', 300);
+INSERT INTO Rooms (capacity, type, price) VALUES (2, 'SUITE', 350);
+INSERT INTO Rooms (capacity, type, price) VALUES (3, 'SUITE', 400);
+INSERT INTO Rooms (capacity, type, price) VALUES (1, 'SUITE', 300);
+INSERT INTO Rooms (capacity, type, price) VALUES (2, 'SUITE', 350);
+INSERT INTO Rooms (capacity, type, price) VALUES (3, 'SUITE', 400);
+INSERT INTO Rooms (capacity, type, price) VALUES (1, 'JUNIOR', 200);
+INSERT INTO Rooms (capacity, type, price) VALUES (2, 'JUNIOR', 250);
+INSERT INTO Rooms (capacity, type, price) VALUES (3, 'JUNIOR', 300);
+INSERT INTO Rooms (capacity, type, price) VALUES (1, 'JUNIOR', 200);
+INSERT INTO Rooms (capacity, type, price) VALUES (2, 'JUNIOR', 250);
+INSERT INTO Rooms (capacity, type, price) VALUES (3, 'JUNIOR', 300);
 
 INSERT INTO Orders (user_id, room_id, capacity, type, start_date, end_date, admin_id, price, status)
-VALUES (1, 2, 1, 'STANDARD', '2017-10-13', '2017-10-15', 2, 360, TRUE);
+VALUES (1, 2, 1, 'STANDARD', '2017-10-13', '2017-10-15', 2, 360, true);
 INSERT INTO Orders (user_id, room_id, capacity, type, start_date, end_date, admin_id, price, status)
-VALUES (1, 2, 1, 'STANDARD', '2017-10-17', '2017-10-20', 2, 720, TRUE);
+VALUES (1, 2, 1, 'STANDARD', '2017-10-17', '2017-10-20', 2, 720, true);
+INSERT INTO Orders (user_id, room_id, capacity, type, start_date, end_date, admin_id, price, status)
+VALUES (1, 2, 1, 'STANDARD', '2017-10-23', '2017-10-25', 1, 720, false);
 
